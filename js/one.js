0a13a8df2c4ef2f73f6a7ed1ee5f0a2e138239f3
xchanin <shannonwbruns@gmail.com>
2022-04-21 18:57:22 -0600

    input output array values

--- js/one.js
+++ js/one.js
@@ -39,4 +39,44 @@ const posOfDupSimpler = (str) => {
     return `first duplicated character is \'${ dups[0].itm }\', first duplicated index is ${ dups[0].index }`;
 }
 
-console.log(posOfDupSimpler('shannon'));
+// console.log(posOfDupSimpler('shannon'));
+
+
+/**
+ * Concat two arrays and find the unique value(s)
+ */
+
+const concatArrOne = [1,2,3,4,5];
+const concatArrTwo = [1,2,3,4,5,6];
+const concatArrs = concatArrOne.concat(concatArrTwo);
+const uniqueVals = [...new Set(concatArrs)];
+// console.log(uniqueVals);
+
+/**
+ * Input array [3,4,5], output array [20,15,12]
+ */
+
+ const iot = (arr) => {
+    
+    let multiplier = arr.splice(0,1)[0];
+    let sums = [];
+
+    return multiply(arr).reverse();
+
+    function multiply(arr) {
+
+        for (let i = 0; i < arr.length; i++) {
+            sums.push(multiplier * arr[i]);
+
+            if (i === arr.length - 1) {
+                multiplier = arr.splice(0,1)[0];
+                multiply(arr);
+            }
+        }
+
+        return sums;
+    }
+ }
+
+
+console.log(iot([3,4,5]));
